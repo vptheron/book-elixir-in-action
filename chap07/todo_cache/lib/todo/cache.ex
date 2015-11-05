08c787a8ce11dfc39e109eae4d26bafe7647f2f5
Vincent Theron <vptheron@gmail.com>
2015-11-05 17:39:20 -0500

    add todo cache

---
+++ chap07/todo_cache/lib/todo/cache.ex
@@ -0,0 +1,37 @@
+defmodule Todo.Cache do
+  use GenServer
+
+  def start do
+  	GenServer.start(__MODULE__, nil)
+  end
+
+  def server_process(pid, list_name) do
+  	GenServer.call(pid, {:server_process, list_name})
+  end
+
+  # Callbacks
+
+  def init(_) do
+  	{:ok, HashDict.new}
+  end
+
+  def handle_call(
+  	{:server_process, list_name},
+  	_,
+  	todo_servers) do
+
+     case HashDict.fetch(todo_servers, list_name) do
+       {:ok, todo_server} -> 
+         {:reply, todo_server, todo_servers}
+       
+       :error ->
+         {:ok, new_server} = Todo.Server.start
+         {
+          :reply, 
+          new_server, 
+          HashDict.put(todo_servers, list_name, new_server)
+         }
+     end
+  end
+
+end
